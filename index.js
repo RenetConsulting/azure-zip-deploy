6c074c90428976ff7bf3a9cee96f9d3bf9be5f87
Sander Rijken <sr@d-90.nl>
2019-11-19 11:42:43 +0100

    use zip deploy service

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const xml2json = require('xml2json-light');
 try {
   const zipFile = core.getInput('zip-file');
   const publishProfile = core.getInput('publish-profile');
-  const scheduleName = core.getInput('schedule-name');
-  const webJobName = core.getInput('webjob-name');
 
   const profile = xml2json.xml2json(publishProfile);
   const msDeployProfile = profile.publishData.publishProfile.find(x => x.publishMethod === 'MSDeploy');
@@ -18,11 +16,9 @@ try {
 
   const authHeader = `Basic ${Buffer.from(`${userName}:${password}`).toString('base64')}`;
 
-  const apiUrl = `${msDeployProfile.destinationAppUrl}/api`;
+  const apiUrl = `https://${msDeployProfile.publishUrl}/api/zipdeploy`;
 
-  const url = `${apiUrl}/zip/site/wwwroot/App_Data/jobs/${scheduleName}/${webJobName}`;
-
-  fs.createReadStream(zipFile).pipe(request.put(url, {
+  fs.createReadStream(zipFile).pipe(request.post(apiUrl, {
     headers: {
       Authorization: authHeader
     }
